6e2ed5cf3825071eac1732364b158077baa96a9f
Sami EZZEROUALI <sami.ezzerouali@hotmail.com>
2019-01-14 12:13:23 +0100

    +ihm_vierge

--- temp_ser/temp_ser.ino
+++ temp_ser/temp_ser.ino
@@ -1,4 +1,4 @@
-int step_time=60;
+int step_time=60*10; // en secondes
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(9600);
@@ -10,4 +10,5 @@ void loop() {
   float temperature_celcius = valeur_brute * (5.0 / 1023.0 * 100.0);
 
   Serial.println(temperature_celcius);
+  delay(1000);
 }
